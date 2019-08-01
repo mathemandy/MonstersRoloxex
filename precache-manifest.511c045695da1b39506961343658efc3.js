5d4f395664db4232b63f36c8d8c2cb306e29cdc0
Andy Eshiet <eandy@groove.ng>
2019-08-01 01:19:09 +0100

    Updates

---
+++ precache-manifest.511c045695da1b39506961343658efc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a2594e26b6bc321dd4b63626e6d1a8",
+    "url": "/MonstersRoloxex/index.html"
+  },
+  {
+    "revision": "d73adfc3c3c292379a7a",
+    "url": "/MonstersRoloxex/static/css/main.db5bff5e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/MonstersRoloxex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d73adfc3c3c292379a7a",
+    "url": "/MonstersRoloxex/static/js/main.9655359b.chunk.js"
+  },
+  {
+    "revision": "e785865f442b88f6ccfc",
+    "url": "/MonstersRoloxex/static/js/runtime~main.7ebcf365.js"
+  }
+]);
